e98cc3298f47a341cc9e5cdca7b7edc4073beaba
Zichen "Charles" Zhang <52727818+zcczhang@users.noreply.github.com>
2021-09-16 10:25:05 -0500

    version 0.1
    roughly reimplement Smmry

---
+++ MiMIC/run_example.py
@@ -0,0 +1,6 @@
+from MiMIC.summary import Summarize
+import io
+
+text = io.open('/Users/charleszhang/Desktop/MiMIC/docs/text1.txt', 'r', encoding='utf-8').read()
+print(Summarize(text, lang="english").summarize(length=5))
+
