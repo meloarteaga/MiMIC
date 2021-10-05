d28e0e37aef18db05c29d52a4e0bcdea05f8c1b6
Imane Chatri <70664538+ImaneChatri@users.noreply.github.com>
2021-10-04 20:46:01 -0500

    Api set up, endpoints postman tested and working

---
+++ MiMIC/api.py
@@ -0,0 +1,37 @@
+from flask import Flask, request, jsonify
+import nltk
+from summary import Summarize
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=["GET"])
+def test_get():
+    return 'This is a test API call!'
+
+
+@app.route('/post', methods=["POST"])
+def test_post():
+    input_json = request.get_json(force=True)
+    dictToReturn = {'text': input_json['text']}
+    return jsonify(dictToReturn)
+
+
+@app.route('/summarize', methods=["POST", "GET"])
+def summarize():
+    input_text = request.get_json(force=True)
+    summarized_text = Summarize(text=input_text, lang="english").summarize()
+    result_dict = {'summarized text': summarized_text}
+    return jsonify(result_dict)
+
+
+@app.route('/numSent', methods=["POST", "GET"])
+def get_num_sent():
+    input_text = request.get_json(force=True)
+    num_sent = len(nltk.sent_tokenize(input_text))
+    result_dict = {'Number of sentences': num_sent}
+    return jsonify(result_dict)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
